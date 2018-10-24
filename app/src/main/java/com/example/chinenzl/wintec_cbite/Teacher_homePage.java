571d3bfb9beddd29291fdc9faf0e128a72275a55
chinenZL <leizha14@student.wintec.ac.nz>
2018-10-24 15:24:29 +1300

    Initial commit

---
+++ app/src/main/java/com/example/chinenzl/wintec_cbite/Teacher_homePage.java
@@ -0,0 +1,13 @@
+package com.example.chinenzl.wintec_cbite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Teacher_homePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_home_page);
+    }
+}
---
+++ app/src/main/java/com/example/chinenzl/wintec_cbite/splash_Activity.java
@@ -0,0 +1,41 @@
+package com.example.chinenzl.wintec_cbite;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class splash_Activity extends AppCompatActivity {
+
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        iv = (ImageView) findViewById(R.id.imageView);
+
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.splash_transion);
+        iv.startAnimation(myanim);
+
+        final Intent i = new Intent ( this, Student_homePage.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+
+        timer.start();
+    }
+}
