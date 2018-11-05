02939571c42dceab202d1a95a0bd76fd85a2ff63
chinenZL <leizha14@student.wintec.ac.nz>
2018-11-05 18:03:45 +1300

    edit some student version

--- app/src/main/java/com/example/chinenzl/wintec_cbite/Teacher_courseList.java
+++ app/src/main/java/com/example/chinenzl/wintec_cbite/Teacher_courseList.java
@@ -7,14 +7,26 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.chinenzl.utils.DataHelper;
+
 public class Teacher_courseList extends AppCompatActivity {
+
+   // private TextView mTextMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_course_list);
+
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        DataHelper db = DataHelper.getInstance();
+        ListAdapter buckysAdapter = new TeacherCourseAdapter(this, db.dataStudy.arrCourse);
+        ListView buckysListView = (ListView) findViewById(R.id.liamListView);
+        buckysListView.setAdapter(buckysAdapter);
 
     }
 
--- app/src/main/java/com/example/chinenzl/wintec_cbite/Teacher_programmeDetail.java
+++ app/src/main/java/com/example/chinenzl/wintec_cbite/Teacher_programmeDetail.java
@@ -1,18 +1,55 @@
 package com.example.chinenzl.wintec_cbite;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.chinenzl.utils.DataHelper;
 
 public class Teacher_programmeDetail extends AppCompatActivity {
+
+    private TextView mTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_programme_detail);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        DataHelper db = DataHelper.getInstance();
+        ListAdapter buckysAdapter = new TeacherCourseAdapter(this, db.dataStudy.arrCourse);
+        ListView buckysListView = (ListView) findViewById(R.id.liamListView);
+        buckysListView.setAdapter(buckysAdapter);
     }
 
     public void clickNetwork(View view){
 
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    //mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    // mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+
+            }
+            return false;
+        }
+    };
 }
